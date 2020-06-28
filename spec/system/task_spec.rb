d21b2fb2d434d615faa44798c5d02ed9fa1f43d9
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-06-28 12:35:45 +0000

    Add system spec for task

---
+++ spec/system/task_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'タスク管理機能', type: :system do
+  describe 'タスク一覧画面' do
+    before do
+      @task = create(:task)
+    end
+    context 'タスクを作成した場合' do
+      it '作成済みのタスクが表示される' do
+        visit tasks_path
+        expect(page).to have_content("task_title")
+      end
+    end
+  end
+end
