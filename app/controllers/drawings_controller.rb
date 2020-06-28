60760cbd0d6484f633c9ea1684751c870c1b56bb
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-06-28 13:55:02 +0000

    Add new and create actions for drawing

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -1,5 +1,17 @@
 class DrawingsController < ApplicationController
+  before_action :set_drawing, only: %i(show edit update destroy)
+
   def new
     @drawing = Drawing.new
+  end
+
+  def create
+    @drawing = Drawing.new(drawing_params)
+    if @drawing.save
+      flash[:notice] = "図面を登録しました！" #辞書
+      redirect_to drawings_path
+    else
+      render :new
+    end
   end
 end
