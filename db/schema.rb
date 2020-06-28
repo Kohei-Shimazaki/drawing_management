c3ea9d46cfc327079d3affa31f831e0cc1da1e63
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-06-28 12:57:49 +0000

    Add drawings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_051220) do
+ActiveRecord::Schema.define(version: 2020_06_28_125331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "drawings", force: :cascade do |t|
+    t.string "title"
+    t.integer "drawing_number"
+    t.text "explanation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
---
+++ spec/factories/drawings.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :drawing do
+    title { "MyString" }
+    drawing_number { 1 }
+    explanation { "MyText" }
+  end
+end
