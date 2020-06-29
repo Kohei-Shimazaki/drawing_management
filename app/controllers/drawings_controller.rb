91301ed51b4b40f34fe961d8e0104c6ac153e0b8
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-06-29 04:17:36 +0000

    Add crud actions for drawings

--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -1,6 +1,10 @@
 class DrawingsController < ApplicationController
   before_action :set_drawing, only: %i(show edit update destroy)
 
+  def index
+    @drawings = Drawing.all
+  end
+
   def new
     @drawing = Drawing.new
   end
@@ -14,4 +18,39 @@ class DrawingsController < ApplicationController
       render :new
     end
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @drawing.update(drawing_params)
+      flash[:notice] = "#{I18n.t("activerecord.models.drawing")}#{I18n.t("flash.update")}"
+      redirect_to drawings_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @drawing.destroy
+    flash[:notice] = "#{I18n.t("activerecord.models.drawing")}#{I18n.t("flash.destroy")}"
+    redirect_to drawings_path
+  end
+
+  private
+    def drawing_params
+      params.require(:drawing).permit(
+        :title,
+        :drawing_number,
+        :explanation,
+      )
+    end
+
+    def set_drawing
+      @drawing = Drawing.find(params[:id])
+    end
+
 end
