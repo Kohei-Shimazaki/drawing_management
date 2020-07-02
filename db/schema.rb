204339b07252d7bdd28016540f90e50fc1a77c58
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-07-02 03:06:30 +0000

    Add foreign_key to comments for question

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_062703) do
+ActiveRecord::Schema.define(version: 2020_07_02_030243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2020_07_01_062703) do
     t.text "attachment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "question_id"
+    t.index ["question_id"], name: "index_comments_on_question_id"
   end
 
   create_table "drawings", force: :cascade do |t|
@@ -83,6 +85,7 @@ ActiveRecord::Schema.define(version: 2020_07_01_062703) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "questions"
   add_foreign_key "questions", "tasks"
   add_foreign_key "revisions", "drawings"
   add_foreign_key "tasks", "drawings"
