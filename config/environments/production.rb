dd0b3a63440c4e62f9c58c15562dea75acec4d12
Kohei-Shimazaki <kohei.shimazaki.k@gmail.com>
2020-08-14 06:44:56 +0000

    Fix cable pass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.action_cable.allowed_request_origins = [ /https?:\/\/.*/ ]
-  config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'ws://54.95.190.19/cable' ]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
