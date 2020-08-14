8134237f0483eb3153cc2efa20931046598f4e9c
Kohei-Shimazaki <kohei.shimazaki.k@gmail.com>
2020-08-14 04:05:17 +0000

    Fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :branch, ENV['BRANCH'] || 'master'
 # deploy先のディレクトリ。
 set :deploy_to, '/var/www/drawing_management'
 # シンボリックリンクをはるフォルダ・ファイル
-set :linked_files, %w{.env config/secrets.yml}
+set :linked_files, %w{.env config/secrets.yml config/master.key}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
 # 保持するバージョンの個数(※後述)
 set :keep_releases, 5
